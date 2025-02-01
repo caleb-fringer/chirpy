11d78e3dbfbfa58bb38d4071f1f842040b13991a
Caleb Fringer <cfringer98@gmail.com>
2025-02-01 12:02:09 -0800

    Finished Ch3 L1

--- main.go
+++ main.go
@@ -4,10 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const PORT = 8080
 const ROOT_PATH = "."
+
+type apiConfig struct {
+	fsHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fsHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fsHits.Store(0)
+}
+
+func (cfg *apiConfig) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d\n", cfg.fsHits.Load())))
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -16,13 +38,19 @@ func main() {
 		Handler: mux,
 	}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(ROOT_PATH))))
+	apiCfg := &apiConfig{}
 
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(ROOT_PATH)))))
+
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("POST /api/reset", apiCfg.reset)
+
+	mux.Handle("GET /api/metrics", apiCfg)
 
 	fmt.Printf("Starting server on port %d...", PORT)
 	log.Fatal(server.ListenAndServe())
